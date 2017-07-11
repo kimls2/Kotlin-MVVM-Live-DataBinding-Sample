f7473022b49aa266d75d1d6b849363d650ab9368
ykim_imac <ykim@qualson.com>
2017-07-11 16:02:24 +0900

    db version 3 to 1

--- app/src/main/java/com/qualson/kotlin_mvvm_live_databinding_sample/data/local/SampleDb.kt
+++ app/src/main/java/com/qualson/kotlin_mvvm_live_databinding_sample/data/local/SampleDb.kt
@@ -7,7 +7,7 @@ import com.qualson.kotlin_mvvm_live_databinding_sample.data.model.User
 /**
  * Created by ykim on 2017. 7. 11..
  */
-@Database(entities = arrayOf(User::class), version = 3)
+@Database(entities = arrayOf(User::class), version = 1)
 abstract class SampleDb : RoomDatabase() {
 
     abstract fun userDao(): UserDao
